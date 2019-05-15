ca00403921f503f52aa41d8c1cd2c9d1deea3d5e
George Go <gngo2018@gmail.com>
2019-05-15 12:02:10 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RequestComponent } from './components/request/request.component';
+import { EquipmentComponent } from './components/equipment/equipment.component';
+import { EquipmentTypeComponent } from './components/equipment-type/equipment-type.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RequestComponent,
+    EquipmentComponent,
+    EquipmentTypeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
